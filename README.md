9a6295a1a1c8b912940058e6911b26cb01c5f17e
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2022-03-22 22:57:47 -0700

    Moved lighty setup info to readme

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # dlang_web_framework
+
+# setup fcgi on ubuntu:
+```sh
+sudo apt-get install lighttpd php5-cgi
+sudo lighty-enable-mod fastcgi
+sudo /etc/init.d/lighttpd force-reload
+```
+
+# change the port in /etc/lighttpd/lighttpd.conf :
+```sh
+server.port               = 90
+```
+
+# change /etc/lighttpd/conf-available/10-fastcgi.conf :
+```sh
+fastcgi.server = ( "/" =>
+	((
+		"max-procs" => 1,
+		"bin-path" => "/home/matt/fastcgi/application",
+		"socket" => "/tmp/application.socket",
+		"check-local" => "disable",
+		"max-request-size" => 100000
+	))
+)
+```
--- source/fcgi.d
+++ source/fcgi.d
@@ -2,28 +2,6 @@
 // This file is licensed under the MIT License
 // https://github.com/workhorsy/dlang_web_framework
 
-/*
-#setup fcgi on ubuntu:
-
-sudo apt-get install lighttpd php5-cgi
-sudo lighty-enable-mod fastcgi
-sudo /etc/init.d/lighttpd force-reload
-
-# change the port in /etc/lighttpd/lighttpd.conf :
-server.port               = 90
-
-# change /etc/lighttpd/conf-available/10-fastcgi.conf :
-fastcgi.server = ( "/" =>
-	((
-		"max-procs" => 1,
-		"bin-path" => "/home/matt/fastcgi/application",
-		"socket" => "/tmp/application.socket",
-		"check-local" => "disable",
-		"max-request-size" => 100000
-	))
-)
-
-*/
 import std.process;
 
 

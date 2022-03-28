e535fdf7d8ee37b0fd09b65afc89660bd2956422
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2022-03-27 17:12:18 -0700

    Moved string to pointer code into MemoryManager

--- boot.js
+++ boot.js
@@ -4,31 +4,43 @@
 // https://github.com/workhorsy/dlang_web_framework
 
 let exports = {};
-let _memory_offset = 0;
 let _element_id = 0;
 let _element_to_id = {};
 
-const js_get_string = (pointer, length) => {
-	let view = new Uint8Array(exports.memory.buffer, pointer, length);
-	let string = new TextDecoder().decode(view);
-	return string;
-};
+class MemoryManager {
+	constructor() {
+		this._memory_offset = 0;
+		this.memory = new WebAssembly.Memory({ initial: 256, maximum: 256, shared: true });
+	}
 
-const js_string_2_wasm_string = (string) => {
-	let bytes = new TextEncoder().encode(string);
-	let buffer = new Uint8Array(exports.memory.buffer, _memory_offset, bytes.byteLength + 1);
-	_memory_offset += bytes.byteLength + 1;
-	buffer.set(bytes);
-	return buffer;
-};
+	pointer_to_string(pointer, length) {
+		let view = new Uint8Array(exports.memory.buffer, pointer, length);
+		let string = new TextDecoder().decode(view);
+		return string;
+	}
+
+	string_to_buffer(string) {
+		let bytes = new TextEncoder().encode(string);
+		let buffer = new Uint8Array(exports.memory.buffer, this._memory_offset, bytes.byteLength + 1);
+		this._memory_offset += bytes.byteLength + 1;
+		buffer.set(bytes);
+		return buffer;
+	}
+
+	free() {
+		this._memory_offset = 0;
+	}
+}
+
+const mm = new MemoryManager();
 
 const js_console_log = (pointer, length) => {
-	let message = js_get_string(pointer, length);
+	let message = mm.pointer_to_string(pointer, length);
 	console.log(message);
 };
 
 const js_document_querySelector = (pointer, length) => {
-	let query = js_get_string(pointer, length);
+	let query = mm.pointer_to_string(pointer, length);
 
 	let element = document.querySelector(query);
 	if (element) {
@@ -42,10 +54,10 @@ const js_document_querySelector = (pointer, length) => {
 };
 
 const js_element_addEventListener = (id, event_name_pointer, event_name_length, function_name_pointer, function_name_length) => {
-	let event_name = js_get_string(event_name_pointer, event_name_length);
+	let event_name = mm.pointer_to_string(event_name_pointer, event_name_length);
 	console.log("!!! event_name: ", event_name);
 
-	let function_name = js_get_string(function_name_pointer, function_name_length);
+	let function_name = mm.pointer_to_string(function_name_pointer, function_name_length);
 	console.log("!!! function_name: ", function_name);
 
 	var element = _element_to_id[id];
@@ -53,7 +65,7 @@ const js_element_addEventListener = (id, event_name_pointer, event_name_length, 
 };
 
 const js_fetch = (id, uri_pointer, uri_length) => {
-	let uri = js_get_string(uri_pointer, uri_length);
+	let uri = mm.pointer_to_string(uri_pointer, uri_length);
 	//console.log("!!!! called js_fetch: ", id, " ", uri_pointer, " ", uri_length);
 	//console.log("!!!! called js_fetch: ", uri);
 	fetch(uri).then(response => {
@@ -62,17 +74,17 @@ const js_fetch = (id, uri_pointer, uri_length) => {
 	}).then(text => {
 		//console.log(uri);
 		//console.log(text);
-		let uri_buffer = js_string_2_wasm_string(uri);
-		let text_buffer = js_string_2_wasm_string(text);
+		let uri_buffer = mm.string_to_buffer(uri);
+		let text_buffer = mm.string_to_buffer(text);
 		//console.log(uri_buffer);
 		//console.log(text_buffer);
 		exports.fetch_cb(id, uri_buffer.byteOffset, uri_buffer.byteLength, text_buffer.byteOffset, text_buffer.byteLength);
-		_memory_offset = 0;
+		mm.free();
 	});
 };
 
 const import_object = {
-	memory: new WebAssembly.Memory({ initial: 256, maximum: 256, shared: true }),
+	memory: mm.memory,
 	env: {
 		js_console_log,
 		js_document_querySelector,
